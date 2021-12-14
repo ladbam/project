903e26a1f11b07f59ae5f1e67e834d00dac754aa
ladbam <94489659+ladbam@users.noreply.github.com>
2021-12-14 06:59:43 -0500

    Update system-inventory.sh

--- system-inventory.sh
+++ system-inventory.sh
@@ -2,7 +2,7 @@
 
 # Description: Script For System Inventory
 # Author: ladbam Email: kloversolutions@outlook.com
-# Github: https://github.com/ladbam/inventory.git
+# Github: https://github.com/ladbam/project.git
 # Description: Project [System Inventory]
 
 # Author: ladbam Email: kloversolutions@outlook.com
